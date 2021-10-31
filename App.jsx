3709c3d297c5dedce3118581bb878fba9b576ec3
yoshioka <yoshi.is03@gmail.com>
2021-10-31 17:57:27 +0900

    Extract Components.

--- App.jsx
+++ App.jsx
@@ -1,54 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/components/CircleButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
-
-      <View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年10月31日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年10月31日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年10月31日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-      </View>
-
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
 
     </View>
   );
@@ -58,66 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f4f8',
-  },
-  appbar: {
-    backgroundColor: '#467fd3',
-    width: '100%',
-    height: 104,
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#ffffff',
-    fontWeight: 'bold',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  memoListItem: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0,0,0, 0.15),',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  circleButton: {
-    backgroundColor: '#467fd3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-  },
-  circleButtonLabel: {
-    color: '#ffffff',
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
---
+++ src/components/AppBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AppBar() {
+  return (
+    <View style={styles.appbar}>
+      <View style={styles.appbarInner}>
+        <Text style={styles.appbarTitle}>Memo App</Text>
+        <Text style={styles.appbarRight}>ログアウト</Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  appbar: {
+    backgroundColor: '#467fd3',
+    width: '100%',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
+    alignItems: 'center',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
+});
