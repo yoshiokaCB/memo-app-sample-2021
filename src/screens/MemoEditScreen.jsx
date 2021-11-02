493b015ff8ab96f9970f07b499b89b00984e037a
yoshioka <yoshi.is03@gmail.com>
2021-11-02 17:20:10 +0900

    Update memo data on MemoEditScreen.

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, TextInput,
+  StyleSheet, View, TextInput, Alert,
 } from 'react-native';
+import firebase from 'firebase';
+import { shape, string } from 'prop-types';
+
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/keyboardSafeView';
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
+  const { navigation, route } = props;
+  const { id, bodyText } = route.params;
+  const [body, setBody] = useState(bodyText);
+
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      ref.set({
+        bodyText: body,
+        updateAt: new Date(),
+      }, { merge: true })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((error) => {
+          Alert.alert('error', error);
+        });
+      // }, (error) => {
+      //   console.log(error);
+      // });
+    }
+  }
+
   return (
     // <KeyboardAvoidingView style={styles.container} behavior="height">
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput value="お買い物リスト" multiline style={styles.input} />
+        <TextInput
+          value={body}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => { setBody(text); }}
+        />
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        // onPress={() => { navigation.goBack(); }}
+        onPress={() => { handlePress(); }}
       />
     </KeyboardSafeView>
   );
 }
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string, bodyText: string }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
